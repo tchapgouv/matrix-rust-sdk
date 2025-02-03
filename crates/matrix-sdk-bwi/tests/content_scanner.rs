2ed92e9dd2d6dfa101e0618d1842dd4bceaef62f
Felix Bohn <felix.bohn.ext@bwi.de>
2025-02-03 10:17:45 +0100

    test: add integration-tests for content_scanner (MESSENGER-6698)

--- crates/matrix-sdk-bwi/tests/content_scanner.rs
+++ crates/matrix-sdk-bwi/tests/content_scanner.rs
@@ -13,19 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-use matrix_sdk_base::ruma::events::room::MediaSource::Encrypted;
-use matrix_sdk_base::ruma::events::room::{
-    EncryptedFile, EncryptedFileInit, JsonWebKey, JsonWebKeyInit,
-};
-use matrix_sdk_base::ruma::exports::serde_json::json;
-use matrix_sdk_base::ruma::mxc_uri;
-use matrix_sdk_base::ruma::serde::Base64;
-use matrix_sdk_base_bwi::content_scanner::scan_state::BWIScanState;
 use matrix_sdk_bwi::content_scanner::BWIContentScanner;
-use simple_logger::SimpleLogger;
-use std::collections::BTreeMap;
-use wiremock::matchers::{method, path};
-use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 #[ignore]
@@ -43,62 +31,132 @@ async fn test_get_public_key() {
     assert_eq!(public_key.unwrap().0, "6eUf9Oa7j6oEvwHcqElti4qu+t36k7gwdwsA3H+xVAI".to_owned());
 }
 
-fn dummy_jwt() -> JsonWebKey {
-    JsonWebKeyInit {
-        kty: "oct".to_owned(),
-        key_ops: vec!["encrypt".to_owned(), "decrypt".to_owned()],
-        alg: "A256CTR".to_owned(),
-        k: Base64::new("qcHVMSgYg-71CauWBezXI5qkaRb0LuIy-Wx5kIaHMIA".as_bytes().to_vec()),
-        ext: true,
-    }
-    .into()
-}
+#[cfg(test)]
+mod test {
+    use crate::test::helpers::{
+        clean_state_response_body, encrypted_file, infected_state_response_body,
+        mount_public_key_mock, mount_scan_mock,
+    };
+    use matrix_sdk_base::ruma::events::room::MediaSource::Encrypted;
+    use matrix_sdk_base_bwi::content_scanner::scan_state::BWIScanState;
+    use matrix_sdk_bwi::content_scanner::BWIContentScanner;
+    use wiremock::ResponseTemplate;
 
-fn encrypted_file() -> EncryptedFile {
-    EncryptedFileInit {
-        url: mxc_uri!("mxc://localhost/encryptedfile").to_owned(),
-        key: dummy_jwt(),
-        iv: Base64::new("X85+XgHN+HEAAAAAAAAAAA".as_bytes().to_vec()),
-        hashes: BTreeMap::new(),
-        v: "v2".to_owned(),
-    }
-    .into()
-}
+    mod helpers {
+        use matrix_sdk_base::ruma::events::room::{
+            EncryptedFile, EncryptedFileInit, JsonWebKey, JsonWebKeyInit,
+        };
+        use matrix_sdk_base::ruma::mxc_uri;
+        use matrix_sdk_base::ruma::serde::Base64;
+        use serde_json::json;
+        use std::collections::BTreeMap;
+        use wiremock::matchers::{method, path};
+        use wiremock::{Mock, MockServer, ResponseTemplate};
 
-#[tokio::test]
-async fn test_media_scan_trusted() {
-    SimpleLogger::new().env().init().unwrap();
-    // Arrange
-    let mock_server = wiremock::MockServer::builder().start().await;
-    Mock::given(method("GET"))
-        .and(path("_matrix/media_proxy/unstable/public_key"))
-        .respond_with(
-            ResponseTemplate::new(200).set_body_json(
-                json! ({"public_key": "GdwYYj5Ey9O96FMi4DjIhPhY604RuZg2Om98Kqh+3GE"}),
-            ),
-        )
-        .mount(&mock_server)
-        .await;
-    Mock::given(method("POST"))
-        .and(path("/_matrix/media_proxy/unstable/scan_encrypted"))
-        .respond_with(ResponseTemplate::new(200).set_body_json(json! (
-            {
-                "clean": true,
-                "info": "File is clean"
+        pub fn dummy_jwt() -> JsonWebKey {
+            JsonWebKeyInit {
+                kty: "oct".to_owned(),
+                key_ops: vec!["encrypt".to_owned(), "decrypt".to_owned()],
+                alg: "A256CTR".to_owned(),
+                k: Base64::new("qcHVMSgYg-71CauWBezXI5qkaRb0LuIy-Wx5kIaHMIA".as_bytes().to_vec()),
+                ext: true,
             }
-        )))
-        .mount(&mock_server)
+            .into()
+        }
+
+        pub fn encrypted_file() -> EncryptedFile {
+            EncryptedFileInit {
+                url: mxc_uri!("mxc://localhost/encryptedfile").to_owned(),
+                key: dummy_jwt(),
+                iv: Base64::new("X85+XgHN+HEAAAAAAAAAAA".as_bytes().to_vec()),
+                hashes: BTreeMap::new(),
+                v: "v2".to_owned(),
+            }
+            .into()
+        }
+
+        pub async fn mount_public_key_mock(mock_server: &MockServer) {
+            Mock::given(method("GET"))
+                .and(path("_matrix/media_proxy/unstable/public_key"))
+                .respond_with(ResponseTemplate::new(200).set_body_json(
+                    json! ({"public_key": "GdwYYj5Ey9O96FMi4DjIhPhY604RuZg2Om98Kqh+3GE"}),
+                ))
+                .mount(&mock_server)
+                .await;
+        }
+
+        pub async fn mount_scan_mock(mock_server: &MockServer, response: ResponseTemplate) {
+            Mock::given(method("POST"))
+                .and(path("/_matrix/media_proxy/unstable/scan_encrypted"))
+                .respond_with(response)
+                .mount(&mock_server)
+                .await;
+        }
+
+        pub fn clean_state_response_body() -> serde_json::value::Value {
+            json! (
+                {
+                    "clean": true,
+                    "info": "File is clean"
+                }
+            )
+        }
+
+        pub fn infected_state_response_body() -> serde_json::value::Value {
+            json! (
+                {
+                    "clean": false,
+                    "info": "***VIRUS DETECTED***"
+                }
+            )
+        }
+    }
+
+    #[tokio::test]
+    async fn test_media_scan_trusted() {
+        // Arrange
+        let mock_server = wiremock::MockServer::builder().start().await;
+        mount_public_key_mock(&mock_server).await;
+        mount_scan_mock(
+            &mock_server,
+            ResponseTemplate::new(200).set_body_json(clean_state_response_body()),
+        )
         .await;
 
-    let client = reqwest::Client::builder().build().unwrap();
-    let mock_server_url = url::Url::parse(&mock_server.uri()).unwrap();
-    let content_scanner = BWIContentScanner::new_with_url(client, mock_server_url);
+        let client = reqwest::Client::builder().build().unwrap();
+        let mock_server_url = url::Url::parse(&mock_server.uri()).unwrap();
+        let content_scanner = BWIContentScanner::new_with_url(client, mock_server_url);
 
-    // Act
-    let scan_state = content_scanner
-        .scan_attachment_with_content_scanner(Encrypted(Box::from(encrypted_file())))
+        // Act
+        let scan_state = content_scanner
+            .scan_attachment_with_content_scanner(Encrypted(Box::from(encrypted_file())))
+            .await;
+
+        // Assert
+        assert_eq!(scan_state, BWIScanState::Trusted);
+    }
+
+    #[tokio::test]
+    async fn test_media_scan_infected_with_200() {
+        // Arrange
+        let mock_server = wiremock::MockServer::builder().start().await;
+        mount_public_key_mock(&mock_server).await;
+        mount_scan_mock(
+            &mock_server,
+            ResponseTemplate::new(200).set_body_json(infected_state_response_body()),
+        )
         .await;
 
-    // Assert
-    assert_eq!(scan_state, BWIScanState::Trusted);
+        let client = reqwest::Client::builder().build().unwrap();
+        let mock_server_url = url::Url::parse(&mock_server.uri()).unwrap();
+        let content_scanner = BWIContentScanner::new_with_url(client, mock_server_url);
+
+        // Act
+        let scan_state = content_scanner
+            .scan_attachment_with_content_scanner(Encrypted(Box::from(encrypted_file())))
+            .await;
+
+        // Assert
+        assert_eq!(scan_state, BWIScanState::Infected);
+    }
 }
